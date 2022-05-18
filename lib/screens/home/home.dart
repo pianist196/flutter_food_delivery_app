af7f5c739f9bca17fa6e93f7d64f922ae56d5bea
pianist196 <1234569050@rambler.ru>
2022-05-18 13:25:19 +0500

    sdsd

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_food_delivery_app/constans/colors.dart';
-import 'package:flutter_food_delivery_app/widgets/restaurant_info.dart';
+import 'package:flutter_food_delivery_app/models/restaurant.dart';
+import 'package:flutter_food_delivery_app/screens/home/widget/food_list.dart';
+import 'package:flutter_food_delivery_app/screens/home/widget/restaurant_info.dart';
 import '../../widgets/custom_app_bar.dart';
 
 class HomePage extends StatefulWidget {
@@ -12,6 +14,8 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   var selected = 0;
+  final pageController = PageController();
+  final restaurant = Restaurant.generateRestaurant();
 
   @override
   Widget build(BuildContext context) {
@@ -19,11 +23,22 @@ class _HomePageState extends State<HomePage> {
       backgroundColor: kBackground,
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
-        children: const [
+        children: [
           CustomAppBar(
-              leftIcon: Icons.arrow_back_ios_outlined,
-              rightIcon: Icons.search_outlined),
-          RestaurantInfo()
+            leftIcon: Icons.arrow_back_ios_outlined,
+            rightIcon: Icons.search_outlined,
+          ),
+          RestaurantInfo(),
+          FoodList(
+            selected: selected,
+            callback: (int index) {
+              setState(() {
+                selected = index;
+              });
+              pageController.jumpToPage(index);
+            },
+            restaurant: restaurant,
+          )
         ],
       ),
     );
